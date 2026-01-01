c7d96581bb5d31d4049c7730ed348aacb8573668
ersaurabhmca <ersaurabhsharma.mca@gmail.com>
2026-01-01 20:28:53 +0530

    Create AesDemoApplication.java

---
+++ src/main/java/com/example/aesdemo/AesDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.aesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AesDemoApplication.class, args);
+    }
+}
